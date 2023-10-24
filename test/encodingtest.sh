fe46f5b31da72f3bb640eedd430bce2da4c77885
Ralf Stemmer <ralf.stemmer@gmx.net>
2023-10-24 13:40:02 +0200

    Using different compiler flags for MacOS builds

--- test/encodingtest.sh
+++ test/encodingtest.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-HEADER="-I.."
-LIBS="-lprinthex"
+if [ "$(uname)" == "Darwin" ]; then
+    HEADER="-I.. -I/usr/local/include"
+    LIBS="-L/usr/local/lib -liconv -lprinthex"
+else
+    HEADER="-I.."
+    LIBS="-lprinthex"
+fi
 
 clang -DxDEBUG -Wno-multichar --std=gnu99 $HEADER $LIBS -O2 -g -o "encodingtest" ./encodingtest.c ../encoding/text.c ../encoding/crc.c
 ./encodingtest
